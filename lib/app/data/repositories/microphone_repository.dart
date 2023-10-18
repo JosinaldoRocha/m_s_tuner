fc4bd207621d069fad02d60748442045a89d5b26
JosinaldoRocha <josinaldorochaleal@hotmail.com>
2023-10-18 15:14:12 -0300

    fix: error correction

--- lib/app/data/repositories/microphone_repository.dart
+++ lib/app/data/repositories/microphone_repository.dart
@@ -2,18 +2,19 @@ import 'package:permission_handler/permission_handler.dart';
 
 class MicrophoneRepository {
   Future<PermissionStatus> microphonePermission() async {
+    await Future.delayed(const Duration(seconds: 1));
     var permission = await Permission.microphone.status;
     if (permission.isDenied) {
       permission = await Permission.microphone.request();
       if (permission.isDenied) {
-        return PermissionStatus.denied;
+        permission = PermissionStatus.denied;
       }
     }
 
     if (permission == PermissionStatus.permanentlyDenied) {
-      return PermissionStatus.permanentlyDenied;
+      permission = PermissionStatus.permanentlyDenied;
     }
 
-    return PermissionStatus.granted;
+    return permission;
   }
 }
--- lib/app/presentation/tune/views/widgets/tune_component.dart
+++ lib/app/presentation/tune/views/widgets/tune_component.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fft/flutter_fft.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:m_s_afinador/app/presentation/tune/providers/tune_provider.dart';
-import 'package:m_s_afinador/app/presentation/tune/views/widgets/select_instrument_widget.dart';
-import 'package:m_s_afinador/app/presentation/tune/views/widgets/string_cipher_widget.dart';
-import 'package:m_s_afinador/app/widgets/icon_widget.dart';
-import '../../states/list_tuning/list_tuning_types_state.dart';
-import 'tuning_bar_widget.dart';
+import 'package:permission_handler/permission_handler.dart';
+import '../../../../constants/instrument_model.dart';
+import '../../../../presentation/tune/providers/tune_provider.dart';
+import '../../../../presentation/tune/views/widgets/select_instrument_widget.dart';
+import '../../states/list_instruments/list_instruments_state.dart';
+import '../../states/microphone_permission/microphone_permission_state.dart';
 import 'tuning_types_widget.dart';
 
 class TuneComponent extends ConsumerStatefulWidget {
@@ -16,38 +17,87 @@ class TuneComponent extends ConsumerStatefulWidget {
 }
 
 class _TuneComponentState extends ConsumerState<TuneComponent> {
+  double? frequency;
+  FlutterFft flutterFft = FlutterFft();
+
+  _initialize() async {
+    await flutterFft.startRecorder();
+
+    flutterFft.onRecorderStateChanged.listen(
+      (data) => {
+        if (mounted)
+          {
+            setState(() => frequency = data[1] as double),
+          },
+        flutterFft.setFrequency = frequency!,
+      },
+      onError: (err) {},
+      onDone: () => {},
+    );
+  }
+
   @override
   void initState() {
     super.initState();
-    Future.microtask(() => ref.read(listTuningTypesProvider.notifier).load());
+    Future.microtask(() {
+      ref.read(listInstrumentsProvider.notifier).loadInstruments();
+      ref.read(microphoneProvider.notifier).loadPermission();
+      frequency = flutterFft.getFrequency;
+    });
+  }
+
+  @override
+  void dispose() {
+    if (flutterFft.getIsRecording) {
+      flutterFft.stopRecorder();
+    }
+    super.dispose();
+  }
+
+  void onTap(value) {
+    setState(() {
+      initialInstrument = value;
+    });
+  }
+
+  void microphoneListen() {
+    ref.listen<MicrophonePermissionState>(
+      microphoneProvider,
+      (previous, next) {
+        if (next is SuccessMicrophonePermissionState) {
+          _initialize();
+        }
+        if (next is FailureMicrophonePermissionState) {
+          showDialog(
+            context: context,
+            builder: (context) => _buildAlertDialog(next, context),
+          );
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(listTuningTypesProvider);
+    final state = ref.watch(listInstrumentsProvider);
+    microphoneListen();
 
-    if (state is LoadingListTuningTypesState) {
+    if (state is LoadingListInstrumentsState) {
       return const Center(
         child: CircularProgressIndicator(),
       );
-    } else if (state is SuccessListTuningTypesState) {
+    } else if (state is SuccessListInstrumentsState) {
       return Container(
         width: double.infinity,
         padding: const EdgeInsets.only(top: 53),
         child: Column(
           children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconWidget(image: 'assets/icons/guitar.png'),
-                SizedBox(width: 4),
-                //DropdownButtonWidget(),
-                SelectInstrumentWidget(),
-              ],
+            SelectInstrumentWidget(
+              instrumentList: state.data,
+              instrument: initialInstrument,
+              onTap: onTap,
             ),
-            const SizedBox(height: 25),
-            TuningTypesWidget(tuningTypes: state.data),
-            const StringCipherWidget(),
+            TuningTypesWidget(instrument: initialInstrument),
             const Text(
               'In tune!',
               textAlign: TextAlign.center,
@@ -57,22 +107,55 @@ class _TuneComponentState extends ConsumerState<TuneComponent> {
                 height: 0,
               ),
             ),
-            const Text(
-              '196.00 Hz',
+            Text(
+              frequency != null ? frequency!.toStringAsFixed(2) : '0',
               textAlign: TextAlign.center,
-              style: TextStyle(
+              style: const TextStyle(
                 color: Color(0xFFC2C2C2),
                 fontSize: 14,
                 height: 0,
               ),
             ),
             const SizedBox(height: 19),
-            const TuningBarWidget(),
           ],
         ),
       );
     } else {
       return Container();
     }
+  }
+
+  AlertDialog _buildAlertDialog(
+      FailureMicrophonePermissionState next, BuildContext context) {
+    return AlertDialog(
+      contentPadding: const EdgeInsets.all(15),
+      content: SizedBox(
+        height: 180,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              next.errorMessage,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onPressed: () async {
+                Navigator.pop(context);
+                await openAppSettings();
+              },
+              child: const Text('Ir para as configurações'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
